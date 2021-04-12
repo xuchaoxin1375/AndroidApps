3c2440f39808a12b04d617c9841fcb3beaf856bd
xuchaoxin <838808930@qq.com>
2021-04-12 16:27:26 +0800

    add several projects

---
+++ saveFile/app/src/main/java/com/zjgsu/xcx/savefiles/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.zjgsu.xcx.savefiles
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import java.io.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var messageText: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        messageText = findViewById(R.id.msgText)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val content = messageText.text.toString()
+        save(content)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val content = load()
+        if (content.isNotEmpty()) {
+            messageText.setText(content)
+        }
+    }
+
+    fun save(inputText: String) {
+        try {
+            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+            writer.use {
+                it.write(inputText)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        /*save as shared files(xml)*/
+//        val editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit()
+//        editor.putString("name", "Tom")
+//        editor.putInt("age", 28)
+//        editor.putBoolean("married", false)
+//        editor.apply()
+    }
+
+    fun load(): String {
+        val content = StringBuilder()
+        try {
+            val input = openFileInput("data")
+            val reader = BufferedReader(InputStreamReader(input))
+            reader.use{
+                reader.use{
+                    reader.forEachLine {
+                        content.append(it)
+                    }
+                }
+            }
+
+        }
+        catch(e:IOException){
+            e.printStackTrace()
+        }
+        return content.toString()
+    }
+}
